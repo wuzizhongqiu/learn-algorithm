e3e4ccd7018167b731c4b9e9a8703d0769f06f6c
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-10-11 18:38:37 +0800

    增加了双指针 复写零 的代码

--- 双指针/code/code.go
+++ 双指针/code/code.go
@@ -1,7 +1,7 @@
 package code
 
 // 移动零
-func moveZeroes(nums []int)  {
+func moveZeroes(nums []int) {
 	slow, fast := 0, 0
 	for fast < len(nums) {
 		if nums[fast] != 0 {
@@ -11,5 +11,32 @@ func moveZeroes(nums []int)  {
 			slow++
 		}
 		fast++
+	}
+}
+
+// 复写零
+func duplicateZeros(arr []int) {
+	left, right := 0, 0
+	// 找到一共经过了几个 0, 调整好位置
+	for right < len(arr) {
+		if arr[left] == 0 { // 注意这里用的是 left
+			right++
+		}
+		left++
+		right++
+	}
+	left--
+	right--
+	// 反向遍历
+	for left >= 0 {
+		if right < len(arr) {
+			arr[right] = arr[left]
+		}
+		if arr[left] == 0 { // 复写 0 的操作
+			right--
+			arr[right] = 0
+		}
+		left--
+		right--
 	}
 }
