8df7f5fc8d27e56a125eb3227402523ba2626ae4
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-10-26 18:16:55 +0800

    channel 实现互斥

--- syncTest/channel/channel.go
+++ syncTest/channel/channel.go
@@ -1,9 +1,24 @@
 package channel
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"syncTest/common"
+)
 
 func Channel() {
 	chanLock := make(chan struct{}, 1) // 想要实现互斥，channel 的容量要设置 1
 	var wg sync.WaitGroup
 	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			chanLock <- struct{}{}
+			common.DoAdd()
+			<-chanLock
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("num=%d", common.Num)
 }
