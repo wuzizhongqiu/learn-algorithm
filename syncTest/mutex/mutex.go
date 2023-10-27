7fab2cb40ca3a4eeff98f64fcc1ddc871219a7d0
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-10-27 18:27:30 +0800

    golang 用 mutex 实现互斥

--- syncTest/mutex/mutex.go
+++ syncTest/mutex/mutex.go
@@ -1,1 +1,24 @@
 package mutex
+
+import (
+	"fmt"
+	"sync"
+	"syncTest/common"
+)
+
+func Mutex() {
+	mu := sync.Mutex{}
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			mu.Lock()
+			common.DoAdd()
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("num=%d", common.Num)
+}
