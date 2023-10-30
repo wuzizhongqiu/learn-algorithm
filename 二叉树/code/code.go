c914f04ab777d66ff088d1f7d7d8bedf011fbe4c
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-10-30 17:35:15 +0800

    二叉树算法：前中后序的非递归遍历

--- 二叉树/code/code.go
+++ 二叉树/code/code.go
@@ -1,5 +1,7 @@
 package code
 
+import "container/list"
+
 // 二叉树的前中后序遍历
 
 type TreeNode struct {
@@ -48,4 +50,69 @@ func postorderTraversal(root *TreeNode) (res []int) {
 	}
 	postorder(root)
 	return res
+}
+
+// 二叉树非递归实现：
+
+func PreorderTraversal(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+	st := list.New()
+	st.PushBack(root)
+	for st.Len() > 0 {
+		node := st.Remove(st.Back()).(*TreeNode)
+		ans = append(ans, node.Val)
+		if node.Right != nil {
+			st.PushBack(node.Right)
+		}
+		if node.Left != nil {
+			st.PushBack(node.Left)
+		}
+	}
+	return ans
+}
+
+func InorderTraversal(root *TreeNode) (ans []int) {
+	st := list.New()
+	cur := root
+	for cur != nil || st.Len() > 0 {
+		if cur != nil {
+			st.PushBack(cur)
+			cur = cur.Left
+		} else {
+			cur = st.Remove(st.Back()).(*TreeNode)
+			ans = append(ans, cur.Val)
+			cur = cur.Right
+		}
+	}
+	return ans
+}
+
+func PostorderTraversal(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+	st := list.New()
+	st.PushBack(root)
+	for st.Len() > 0 {
+		node := st.Remove(st.Back()).(*TreeNode)
+		ans = append(ans, node.Val)
+		if node.Left != nil {
+			st.PushBack(node.Left)
+		}
+		if node.Right != nil {
+			st.PushBack(node.Right)
+		}
+	}
+	reverse(ans)
+	return ans
+}
+
+func reverse(a []int) { // 反转数组
+	l, r := 0, len(a)-1
+	for l < r {
+		a[l], a[r] = a[r], a[l]
+		l, r = l+1, r-1
+	}
 }
