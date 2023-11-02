5dd14f9c158c4111fc286e032275b5db96384335
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-11-02 12:43:06 +0800

    LeetCode 每日一题 2023_11_2 环和杆

--- LeetCode 每日一题/2023_11/code/code.go
+++ LeetCode 每日一题/2023_11/code/code.go
@@ -57,3 +57,31 @@ func maximumInvitations(favorite []int) int {
 	}
 	return max(maxRingSize, sumChainSize)
 }
+
+// 环和杆
+func countPoints(rings string) (ans int) {
+	num := [10][3]int{}
+	ch := 0
+	for i := 0; i < len(rings); i += 2 {
+		if rings[i] == 'R' {
+			ch = 0
+		} else if rings[i] == 'G' {
+			ch = 1
+		} else if rings[i] == 'B' {
+			ch = 2
+		}
+		num[rings[i+1]-'0'][ch]++
+	}
+	for _, v1 := range num {
+		cnt := 0
+		for _, v2 := range v1 {
+			if v2 > 0 {
+				cnt++
+			}
+		}
+		if cnt == 3 { // 三个标志位都有数，证明集齐了所有颜色
+			ans++
+		}
+	}
+	return ans
+}
