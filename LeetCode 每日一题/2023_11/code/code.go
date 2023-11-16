cf474f78a5e06e9262ff8605fb06837f54c24b67
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-11-16 08:49:09 +0800

    LeetCode 每日一题/2023_11_16 最长奇偶子数组

--- LeetCode 每日一题/2023_11/code/code.go
+++ LeetCode 每日一题/2023_11/code/code.go
@@ -312,3 +312,22 @@ func maximizeSum(nums []int, k int) int {
 	}
 	return (maxV + (maxV + k - 1)) * k / 2
 }
+
+// 最长奇偶子数组
+func longestAlternatingSubarray(nums []int, threshold int) (ans int) {
+	n := len(nums)
+	i := 0
+	for i < n {
+		if nums[i] > threshold || nums[i]%2 != 0 { // 寻找起始点
+			i++
+			continue
+		}
+		start := i
+		i++ // 从第二个点开始找
+		for i < n && nums[i] <= threshold && nums[i]%2 != nums[i-1]%2 {
+			i++
+		}
+		ans = max(ans, i-start)
+	}
+	return ans
+}
