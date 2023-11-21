bc8b32a19ed291d329c156083790ffe8f9021f4b
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-11-21 12:33:51 +0800

    LeetCode 每日一题/2023_11_21 美化数组的最少删除数

--- LeetCode 每日一题/2023_11/code/code.go
+++ LeetCode 每日一题/2023_11/code/code.go
@@ -359,3 +359,18 @@ func maxSubArray(nums []int) int {
 	}
 	return ans
 }
+
+// 美化数组的最少删除数
+func minDeletion(nums []int) (cnt int) {
+	for i := 0; i < len(nums); i++ {
+		for i%2 == 0 && i < len(nums)-1 && nums[i] == nums[i+1] {
+			nums = append(nums[:i], nums[i+1:]...)
+			cnt++
+		}
+	}
+	if len(nums)%2 != 0 {
+		nums = nums[:len(nums)-1]
+		cnt++
+	}
+	return cnt
+}
