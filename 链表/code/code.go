adbd26a0e66ba80fd7c78f1b64eeaed7b2cdf999
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-11-26 09:55:26 +0800

    测试 main 包的特性

--- 链表/code/code.go
+++ 链表/code/code.go
@@ -1,9 +1,9 @@
 package code
 
 type ListNode struct {
-	 Val int
-	 Next *ListNode
- }
+	Val  int
+	Next *ListNode
+}
 
 // 移除链表元素
 func removeElements(head *ListNode, val int) *ListNode {
@@ -11,11 +11,12 @@ func removeElements(head *ListNode, val int) *ListNode {
 	cur := phead
 	for cur.Next != nil {
 		tmp := cur
-		cur = cur.Next // 遍历
+		cur = cur.Next      // 遍历
 		if cur.Val == val { // 移除节点的操作
 			tmp.Next = cur.Next
 			cur = tmp
 		}
 	}
+	add()
 	return phead.Next
 }
