2b5ee3b002e68c01c47ab223635fc7b692640741
mid-autumn-festival-of-wuzi <2669560858@qq.com>
2023-12-10 18:36:47 +0800

    70. 爬楼梯 经典 dp

--- LeetCode 每日一题/2023_12/code/code.go
+++ LeetCode 每日一题/2023_12/code/code.go
@@ -166,3 +166,14 @@ func nextBeautifulNumber(n int) int {
 		}
 	}
 }
+
+
+// 70. 爬楼梯 经典 dp
+func climbStairs(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i < n+1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
